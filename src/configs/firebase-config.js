dba2d9bcc66d2b05489c80b74274639d302fbee5
divine <divineobi07@gmail.com>
2023-05-29 17:04:45 +0100

    add firebase package

---
+++ src/configs/firebase-config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGscOuytLSZzgqOa0eDnBfOaNac_WpLeE",
+  authDomain: "retro-ecommerce-7df8d.firebaseapp.com",
+  projectId: "retro-ecommerce-7df8d",
+  storageBucket: "retro-ecommerce-7df8d.appspot.com",
+  messagingSenderId: "710915029167",
+  appId: "1:710915029167:web:9e481cd06ef2d4a9603c5a",
+  measurementId: "G-PBNFHB4MEJ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
