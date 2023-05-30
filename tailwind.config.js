bc989ffeb625071a53f605c92f07b459ef8090a1
divine <divineobi07@gmail.com>
2023-05-30 10:44:38 +0100

    chore: update tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    backgroundColor: {
+      white: "#fff",
+      black: "#000000;",
+    },
+    fontFamily: {
+      Inter: ["Inter, sans-serif"],
+    },
+    fontWeight: {
+      400: "400",
+      500: "500",
+    },
+    fontSize: {
+      0.75: "0.75rem",
+      0.875: "0.875rem",
+      1: "1rem",
+    },
     extend: {},
   },
   plugins: [],
 };
-
