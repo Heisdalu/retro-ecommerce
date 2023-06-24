611ff839f15d77a4b082a0afca0a0dc66833a23d
divine <divineobi07@gmail.com>
2023-06-24 23:28:46 +0100

    add favorite links

--- src/components/FavoriteList/FavoriteList.jsx
+++ src/components/FavoriteList/FavoriteList.jsx
@@ -1,34 +1,58 @@
+import { Link } from "react-router-dom";
+
 const FavoriteList = () => {
   return (
     <div className="overflow-auto pl-1 pb-0.5 md:overflow-hidden sticky">
       <ul className="w-slide flex space-x-[0.8rem] text-black md:w-[700px] md:flex-wrap md:mx-auto justify-center md:items-center">
-        <li className="py-[6px] px-[12px] border-1 centerPos rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        <Link
+          to="/search/cafe"
+          className="py-[6px] px-[12px] border-1 centerPos rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Cafe
-        </li>
-        <li
+        </Link>
+        <Link
+          to="/search/coffee"
           className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75
       md:mt-1 md:text-1"
         >
           Coffee $ Juice Bar
-        </li>
-        <li className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="/search/vegetables"
+          className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Fruits & Vegetables
-        </li>
-        <li className="centerPos  py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="meats"
+          className="centerPos  py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Meats & Seaford
-        </li>
-        <li className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="snacks"
+          className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Snacks
-        </li>
-        <li className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="beverages"
+          className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Beverages
-        </li>
-        <li className="centerPos  py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="/search/vegetables"
+          className="centerPos  py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Alcohol
-        </li>
-        <li className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1">
+        </Link>
+        <Link
+          to="/search/frozen"
+          className="centerPos py-[6px] px-[11px] border-1 rounded-rx3 text-0.75 md:mt-1 md:text-1"
+        >
           Frozen
-        </li>
+        </Link>
       </ul>
     </div>
   );
